3034329d88f5fc37c7be1161f90e1a1649683a6c
lianaliu-waiward <Liana.Liu@waiward.com>
2021-03-30 20:29:04 -0600

    Add Current Date
    Fixed AB#54

--- HelloWorld/Form1.Designer.vb
+++ HelloWorld/Form1.Designer.vb
@@ -23,12 +23,14 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblDate = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.Dock = System.Windows.Forms.DockStyle.Top
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Blue
         Me.Label1.Location = New System.Drawing.Point(0, 0)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(800, 108)
@@ -36,11 +38,23 @@ Partial Class Form1
         Me.Label1.Text = "Hello World"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
+        'lblDate
+        '
+        Me.lblDate.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDate.Location = New System.Drawing.Point(0, 108)
+        Me.lblDate.Name = "lblDate"
+        Me.lblDate.Size = New System.Drawing.Size(800, 342)
+        Me.lblDate.TabIndex = 1
+        Me.lblDate.Text = "Date"
+        Me.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblDate)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Hello World"
@@ -49,4 +63,5 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents lblDate As Label
 End Class
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # HelloWorld
 
 Update #1 This project is created for testing GitHub and Azure Repo
+
+Update #2 Add current date in project
